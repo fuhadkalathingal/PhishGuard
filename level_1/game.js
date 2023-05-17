6b671f292ea270d84cd5e32cad57de4cbd430a81
imuhammedfuhad <muhammedfuhad@finalgoals.in>
2023-05-18 04:05:28 +0530

    completed level 1

---
+++ level_1/game.js
@@ -0,0 +1,86 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+    {
+        question: "Pick the correct option based on the amazon voucher send by Sam 💬",
+        choice1: 'It is real, I want the reward',
+        choice2: 'No, Its not safe. Its a phishing link',
+        answer: 2,
+    }
+]
+
+const SCORE_POINTS = 200
+const MAX_QUESTIONS = 1
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startGame()
---
+++ level_1/script.js
@@ -0,0 +1,60 @@
+/* Time */
+
+var deviceTime = document.querySelector('.status-bar .time');
+var messageTime = document.querySelectorAll('.message .time');
+
+deviceTime.innerHTML = moment().format('h:mm');
+
+setInterval(function() {
+	deviceTime.innerHTML = moment().format('h:mm');
+}, 1000);
+
+for (var i = 0; i < messageTime.length; i++) {
+	messageTime[i].innerHTML = moment().format('h:mm A');
+}
+
+/* Message */
+
+var form = document.querySelector('.conversation-compose');
+var conversation = document.querySelector('.conversation-container');
+
+form.addEventListener('submit', newMessage);
+
+function newMessage(e) {
+	var input = e.target.input;
+
+	if (input.value) {
+		var message = buildMessage(input.value);
+		conversation.appendChild(message);
+		animateMessage(message);
+	}
+
+	input.value = '';
+	conversation.scrollTop = conversation.scrollHeight;
+
+	e.preventDefault();
+}
+
+function buildMessage(text) {
+	var element = document.createElement('div');
+
+	element.classList.add('message', 'sent');
+
+	element.innerHTML = text +
+		'<span class="metadata">' +
+			'<span class="time">' + moment().format('h:mm A') + '</span>' +
+			'<span class="tick tick-animation">' +
+				'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck" x="2047" y="2061"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#92a58c"/></svg>' +
+				'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#4fc3f7"/></svg>' +
+			'</span>' +
+		'</span>';
+
+	return element;
+}
+
+function animateMessage(message) {
+	setTimeout(function() {
+		var tick = message.querySelector('.tick');
+		tick.classList.remove('tick-animation');
+	}, 500);
+}
