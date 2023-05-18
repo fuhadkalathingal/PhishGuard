befe9f7bd534071a459db45811a7eddf1bac7f8b
imuhammedfuhad <muhammedfuhad@finalgoals.in>
2023-05-18 06:31:57 +0530

    the chat with sam has completly changed

--- level_1/script.js
+++ level_1/script.js
@@ -1,60 +1,94 @@
-/* Time */
+$chat = $(".chat");
+$profile = $(".user-profile");
 
-var deviceTime = document.querySelector('.status-bar .time');
-var messageTime = document.querySelectorAll('.message .time');
+/* ===================================
+    Screen resize handler
+====================================== */
+const smallDevice = window.matchMedia("(max-width: 767px)");
+const largeScreen = window.matchMedia("(max-width: 1199px)");
+smallDevice.addEventListener("change", handleDeviceChange);
+largeScreen.addEventListener("change", handleLargeScreenChange);
 
-deviceTime.innerHTML = moment().format('h:mm');
+handleDeviceChange(smallDevice);
+handleLargeScreenChange(largeScreen);
 
-setInterval(function() {
-	deviceTime.innerHTML = moment().format('h:mm');
-}, 1000);
-
-for (var i = 0; i < messageTime.length; i++) {
-	messageTime[i].innerHTML = moment().format('h:mm A');
+function handleDeviceChange(e) {
+  if (e.matches) chatMobile();
+  else chatDesktop();
 }
 
-/* Message */
-
-var form = document.querySelector('.conversation-compose');
-var conversation = document.querySelector('.conversation-container');
-
-form.addEventListener('submit', newMessage);
-
-function newMessage(e) {
-	var input = e.target.input;
-
-	if (input.value) {
-		var message = buildMessage(input.value);
-		conversation.appendChild(message);
-		animateMessage(message);
-	}
-
-	input.value = '';
-	conversation.scrollTop = conversation.scrollHeight;
-
-	e.preventDefault();
+function handleLargeScreenChange(e) {
+  if (e.matches) profileToogleOnLarge();
+  else profileExtraLarge();
 }
 
-function buildMessage(text) {
-	var element = document.createElement('div');
-
-	element.classList.add('message', 'sent');
-
-	element.innerHTML = text +
-		'<span class="metadata">' +
-			'<span class="time">' + moment().format('h:mm A') + '</span>' +
-			'<span class="tick tick-animation">' +
-				'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck" x="2047" y="2061"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#92a58c"/></svg>' +
-				'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#4fc3f7"/></svg>' +
-			'</span>' +
-		'</span>';
-
-	return element;
+function chatMobile() {
+  $chat.addClass("chat--mobile");
 }
 
-function animateMessage(message) {
-	setTimeout(function() {
-		var tick = message.querySelector('.tick');
-		tick.classList.remove('tick-animation');
-	}, 500);
+function chatDesktop() {
+  $chat.removeClass("chat--mobile");
 }
+
+function profileToogleOnLarge() {
+  $profile.addClass("user-profile--large");
+}
+
+function profileExtraLarge() {
+  $profile.removeClass("user-profile--large");
+}
+
+/* ===================================
+    Events
+====================================== */
+
+$(".messaging-member").click(function () {
+  $chat.fadeIn();
+  $chat.addClass("chat--show");
+});
+
+$(".chat__previous").click(function () {
+  $chat.removeClass("chat--show");
+});
+
+$(".chat__details").click(function () {
+  $profile.fadeIn();
+  $profile.addClass("user-profile--show");
+});
+
+$(".user-profile__close").click(function () {
+  $profile.removeClass("user-profile--show");
+});
+
+$(".messages-page__dark-mode-toogler").click(function () {
+  $("body").toggleClass("dark-mode");
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const messageInput = document.getElementById("message");
+  const sendButton = document.getElementById("sendButton");
+  const messageList = document.getElementById("messageList");
+
+  sendButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    const messageText = messageInput.value.trim();
+    if (messageText !== "") {
+      const messageItem = document.createElement("li");
+      messageItem.classList.add("chat__list-messages", "li");
+
+      const messageBubble = document.createElement("div");
+      messageBubble.classList.add("chat__bubble", "chat__bubble--me");
+      messageBubble.textContent = messageText;
+
+      messageItem.appendChild(messageBubble);
+      messageList.appendChild(messageItem);
+
+      // Scroll to the bottom of the message list
+      messageList.scrollTop = messageList.scrollHeight;
+
+      // Clear the input field
+      messageInput.value = "";
+    }
+  });
+});
